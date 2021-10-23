e774369fde624ffd702374eed234891543bda9b2
kyle Joest <kjoest92@gmail.com>
2021-10-23 08:57:34 -0500

    Made Grid Cards for Restaurants

--- RestaurantRater/Models/Restaurant.cs
+++ RestaurantRater/Models/Restaurant.cs
@@ -12,6 +12,7 @@ namespace RestaurantRater.Models
         public string Name { get; set; }
         public string Address { get; set; }
         public int Rating { get; set; }
+        public string ImagePath { get; set; }
     }
 
     public class RestaurantDbContext : DbContext
